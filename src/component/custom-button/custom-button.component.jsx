32832caff792d81dcc0e45a1551dec44ec3bd771
Toru Obara <happy_birthday_9_6@yahoo.co.jp>
2020-11-21 15:49:50 +0900

    implemented firebase utils, included ability to store authencated users into firestore

--- src/component/custom-button/custom-button.component.jsx
+++ src/component/custom-button/custom-button.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CutomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
     <button
         className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
         {...otherProps}
@@ -11,4 +11,4 @@ const CutomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
     </button>
 );
 
-export default CutomButton;
+export default CustomButton;
