3da53d4086fc9152332b0734e9cc73b3eae09453
Toru Obara <happy_birthday_9_6@yahoo.co.jp>
2020-12-29 16:01:15 +0900

    Updated Error.

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@ const middlewares = [];
 if (process.env.NODE_ENV === "development") {
     middlewares.push(logger);
 }
-git
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 
